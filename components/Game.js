563a4a43adb5e10c7ff3fdaac15aca047a5fec3d
Sutanwidhara <79209127+Sutanwidhara@users.noreply.github.com>
2024-03-25 18:49:12 +0530

    commit1

---
+++ components/Game.js
@@ -0,0 +1,13 @@
+AFRAME.registerComponent("game-play",{
+    schema:{elementId:{type:"string",default:"#ring1"}},
+    update : function(){
+        this.isCollided(this.data.elementId)},
+
+    isCollided : function(elementId){
+        const element = document.querySelector(elementId)
+        element.addEventListener("collide",(e)=>{
+            if(elementId.includes("#ring")){
+                console.log(elementId+" collision ")
+            }
+            else if(element.includes("#hurdle")){
+                console.log("bird Collision")}})},})
